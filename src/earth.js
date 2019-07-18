8bf15f6bc0be80099a220e05e1129bf77141b8bf
kuake <kuake@gaoding.com>
2019-07-18 16:19:39 +0800

    chore: update code

--- src/earth.js
+++ src/earth.js
@@ -1,4 +1,5 @@
-import {mat4, initShaders,initTextures, initArrayBuffer, getMvpMatrix} from './utils/index.js'
+import {mat4, initShaders,initTextures, getMvpMatrix, initVertexBuffers} from './utils/index.js'
+import {createSphereModel} from './utils/geometry/index.js';
 
 const VSHADER_SOURCE =`
   attribute vec4 a_Position; 
@@ -47,7 +48,7 @@ const canvas = document.getElementById('webgl');
 const gl = canvas.getContext('webgl');
 
 initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE);
-const n = initVertexBuffers(gl);
+const n = initVertexBuffers(gl, createSphereModel(100));
 
 gl.clearColor(0.0, 0.0, 0.5, 1.0);
 gl.enable(gl.DEPTH_TEST);
@@ -89,69 +90,9 @@ initTextures(gl, [url0]).then(() => {
 function draw(){
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   mat4.rotate(modelMatrix,modelMatrix,Math.PI/360,[0,1,0]);
-  // mat4.rotate(modelMatrix,modelMatrix,Math.PI/360,[0.4013084,0.915943,0]);
   gl.uniformMatrix4fv(u_ModelMatrix,false,modelMatrix);
   gl.uniformMatrix4fv(u_MvpMatrix, false, getMvpMatrix(perspectiveMatrix,viewMatrix, modelMatrix));
   gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_SHORT, 0);
   window.requestAnimationFrame(draw);
 }
 
-
-function initVertexBuffers(gl) {
-  const SPHERE_DIV = 100;
-
-  let i, ai, si, ci;
-  let j, aj, sj, cj;
-  let p1, p2;
-
-  const positions = [];
-  const indices = [];
-  const texCoord = [];
-
-  for (j = 0; j <= SPHERE_DIV; j++) {
-    aj = j * Math.PI / SPHERE_DIV;
-    sj = Math.sin(aj);
-    cj = Math.cos(aj);
-    for (i = 0; i <= SPHERE_DIV; i++) {
-      ai = i * 2 * Math.PI / SPHERE_DIV;
-      si = Math.sin(ai);
-      ci = Math.cos(ai);
-
-      positions.push(si * sj);  // X
-      positions.push(cj);       // Y
-      positions.push(ci * sj);  // Z
-    }
-  }
-
-  for (j = 0; j < SPHERE_DIV; j++) {
-    for (i = 0; i < SPHERE_DIV; i++) {
-      p1 = j * (SPHERE_DIV+1) + i;
-      p2 = p1 + (SPHERE_DIV+1);
-
-      indices.push(p1);
-      indices.push(p2);
-      indices.push(p1 + 1);
-
-      indices.push(p1 + 1);
-      indices.push(p2);
-      indices.push(p2 + 1);
-    }
-  }
-
-  for (j = 0; j<= SPHERE_DIV;j++){
-    for(i = 0; i<= SPHERE_DIV;i++){
-      texCoord.push(i/SPHERE_DIV,1-j/SPHERE_DIV)
-    }
-  }
-
-  if (!initArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3)) return -1;
-  if (!initArrayBuffer(gl, 'a_Normal', new Float32Array(positions), gl.FLOAT, 3))  return -1;
-  if (!initArrayBuffer(gl, 'a_TexCoord', new Float32Array(texCoord),gl.FLOAT,2)) return -1;
-  
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-  
-  return indices.length;
-}
